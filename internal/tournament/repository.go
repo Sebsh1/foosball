31edad647855e726b8bd371bd833b527930b16f4
Sebastian Holmgaard <au590976@uni.au.dk>
2023-03-06 22:58:53 +0100

    add tournament scaffold

--- internal/tournament/repository.go
+++ internal/tournament/repository.go
@@ -1,1 +1,54 @@
 package tournament
+
+import (
+	"context"
+	"foosball/internal/models"
+
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+)
+
+const ErrCodeMySQLDuplicateEntry uint16 = 1062
+
+var (
+	ErrNotFound       = errors.New("not found")
+	ErrDuplicateEntry = errors.New("already exists")
+	ErrNotDeleted     = errors.New("not deleted")
+	ErrNotUpdated     = errors.New("not updated")
+)
+
+type Repository interface {
+	GetTournamen(ctx context.Context, id uint) (*models.Tournament, error)
+	CreateTournament(ctx context.Context, tournament *models.Tournament) error
+	DeleteTournament(ctx context.Context, tournament *models.Tournament) error
+	UpdateTournament(ctx context.Context, tournament *models.Tournament) error
+}
+
+type RepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &RepositoryImpl{
+		db: db,
+	}
+}
+
+func (*RepositoryImpl) CreateTournament(ctx context.Context, tournament *models.Tournament) error {
+	// TODO
+	panic("unimplemented")
+}
+
+func (*RepositoryImpl) DeleteTournament(ctx context.Context, tournament *models.Tournament) error {
+	// TODO
+	panic("unimplemented")
+}
+
+func (*RepositoryImpl) GetTournamen(ctx context.Context, id uint) (*models.Tournament, error) {
+	// TODO
+	panic("unimplemented")
+}
+func (*RepositoryImpl) UpdateTournament(ctx context.Context, tournament *models.Tournament) error {
+	// TODO
+	panic("unimplemented")
+}
--- internal/tournament/service.go
+++ internal/tournament/service.go
@@ -1,1 +1,43 @@
 package tournament
+
+import (
+	"context"
+	"foosball/internal/models"
+)
+
+type Service interface {
+	GetTournament(ctx context.Context, id uint) (*models.Tournament, error)
+	CreateTournament(ctx context.Context, teams []*models.Team) error
+	DeleteTournament(ctx context.Context, id uint) error
+	UpdateTournament(ctx context.Context, tournament *models.Tournament) error
+}
+
+type ServiceImpl struct {
+	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &ServiceImpl{
+		repo: repo,
+	}
+}
+
+func (*ServiceImpl) CreateTournament(ctx context.Context, teams []*models.Team) error {
+	// TODO
+	panic("unimplemented")
+}
+
+func (*ServiceImpl) DeleteTournament(ctx context.Context, id uint) error {
+	// TODO
+	panic("unimplemented")
+}
+
+func (*ServiceImpl) GetTournament(ctx context.Context, id uint) (*models.Tournament, error) {
+	// TODO
+	panic("unimplemented")
+}
+
+func (*ServiceImpl) UpdateTournament(ctx context.Context, tournament *models.Tournament) error {
+	// TODO
+	panic("unimplemented")
+}
