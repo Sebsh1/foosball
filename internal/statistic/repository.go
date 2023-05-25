de3d39f3a6e927f9b05a8d882c86275ca4dddfea
Sebastian Holmgaard <au590976@uni.au.dk>
2023-05-25 23:18:05 +0200

    add statistic methods to support leaderboards

--- internal/statistic/repository.go
+++ internal/statistic/repository.go
@@ -8,6 +8,11 @@ import (
 
 type Repository interface {
 	GetStatisticByUserID(ctx context.Context, userID uint) (*Statistic, error)
+	GetTopXUserIDsByWins(ctx context.Context, topX int) (userIDs []uint, wins []int, err error)
+	GetTopXUserIDsByWinStreaks(ctx context.Context, topX int) (userIDs []uint, winStreaks []int, err error)
+	GetTopXUserIDsByLossStreaks(ctx context.Context, topX int) (userIDs []uint, lossStreaks []int, err error)
+	GetTopXUserIDsByWinLossRatios(ctx context.Context, topX int) (userIDs []uint, winLossRatios []float64, err error)
+	GetTopXUserIDsByMatchesPlayed(ctx context.Context, topX int) (userIDs []uint, matchesPlayed []int, err error)
 	UpdateStatistics(ctx context.Context, stats []Statistic) error
 }
 
@@ -28,6 +33,66 @@ func (r *RepositoryImpl) GetStatisticByUserID(ctx context.Context, userID uint) 
 	}
 
 	return &stats, nil
+}
+
+func (r *RepositoryImpl) GetTopXUserIDsByWins(ctx context.Context, topX int) ([]uint, []int, error) {
+	var userIDs []uint
+	var wins []int
+
+	err := r.db.WithContext(ctx).Model(&Statistic{}).Order("wins desc").Limit(topX).Pluck("user_id", &userIDs).Pluck("wins", &wins).Error
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return userIDs, wins, nil
+}
+
+func (r *RepositoryImpl) GetTopXUserIDsByWinStreaks(ctx context.Context, topX int) ([]uint, []int, error) {
+	var userIDs []uint
+	var winStreaks []int
+
+	err := r.db.WithContext(ctx).Model(&Statistic{}).Order("win_streak desc").Limit(topX).Pluck("user_id", &userIDs).Pluck("win_streak", &winStreaks).Error
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return userIDs, winStreaks, nil
+}
+
+func (r *RepositoryImpl) GetTopXUserIDsByLossStreaks(ctx context.Context, topX int) ([]uint, []int, error) {
+	var userIDs []uint
+	var lossStreaks []int
+
+	err := r.db.WithContext(ctx).Model(&Statistic{}).Order("loss_streak desc").Limit(topX).Pluck("user_id", &userIDs).Pluck("loss_streak", &lossStreaks).Error
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return userIDs, lossStreaks, nil
+}
+
+func (r *RepositoryImpl) GetTopXUserIDsByWinLossRatios(ctx context.Context, topX int) ([]uint, []float64, error) {
+	var userIDs []uint
+	var winLossRatios []float64
+
+	err := r.db.WithContext(ctx).Model(&Statistic{}).Order("win_loss_ratio desc").Limit(topX).Pluck("user_id", &userIDs).Pluck("win_loss_ratio", &winLossRatios).Error
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return userIDs, winLossRatios, nil
+}
+
+func (r *RepositoryImpl) GetTopXUserIDsByMatchesPlayed(ctx context.Context, topX int) ([]uint, []int, error) {
+	var userIDs []uint
+	var matchesPlayed []int
+
+	err := r.db.WithContext(ctx).Model(&Statistic{}).Order("matches_played desc").Limit(topX).Pluck("user_id", &userIDs).Pluck("matches_played", &matchesPlayed).Error
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return userIDs, matchesPlayed, nil
 }
 
 func (r *RepositoryImpl) UpdateStatistics(ctx context.Context, stats []Statistic) error {
--- internal/statistic/service.go
+++ internal/statistic/service.go
@@ -7,6 +7,11 @@ import (
 )
 
 type Service interface {
+	GetTopXUserIDsByWins(ctx context.Context, topX int) (userIDs []uint, wins []int, err error)
+	GetTopXUserIDsByWinStreak(ctx context.Context, topX int) (userIDs []uint, winStreaks []int, err error)
+	GetTopXUserIDsByLossStreak(ctx context.Context, topX int) (userIDs []uint, lossStreaks []int, err error)
+	GetTopXUserIDsByWinLossRatio(ctx context.Context, topX int) (userIDs []uint, winLossRatios []float64, err error)
+	GetTopXUserIDsByMatchesPlayed(ctx context.Context, topX int) (userIDs []uint, matchesPlayed []int, err error)
 	UpdateStatisticsByUserIDs(ctx context.Context, userIDs []uint, result MatchResult) error
 }
 
@@ -20,6 +25,51 @@ func NewService(repo Repository) Service {
 	}
 }
 
+func (s *ServiceImpl) GetTopXUserIDsByWins(ctx context.Context, topX int) ([]uint, []int, error) {
+	userIDs, wins, err := s.repo.GetTopXUserIDsByWins(ctx, topX)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "failed to get top %d userIDs by wins", topX)
+	}
+
+	return userIDs, wins, nil
+}
+
+func (s *ServiceImpl) GetTopXUserIDsByWinStreak(ctx context.Context, topX int) ([]uint, []int, error) {
+	userIDs, winStreaks, err := s.repo.GetTopXUserIDsByWinStreaks(ctx, topX)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "failed to get top %d userIDs by win streaks", topX)
+	}
+
+	return userIDs, winStreaks, nil
+}
+
+func (s *ServiceImpl) GetTopXUserIDsByLossStreak(ctx context.Context, topX int) ([]uint, []int, error) {
+	userIDs, lossStreaks, err := s.repo.GetTopXUserIDsByLossStreaks(ctx, topX)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "failed to get top %d userIDs by loss streaks", topX)
+	}
+
+	return userIDs, lossStreaks, nil
+}
+
+func (s *ServiceImpl) GetTopXUserIDsByWinLossRatio(ctx context.Context, topX int) ([]uint, []float64, error) {
+	userIDs, winLossRatios, err := s.repo.GetTopXUserIDsByWinLossRatios(ctx, topX)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "failed to get top %d userIDs by win loss ratio", topX)
+	}
+
+	return userIDs, winLossRatios, nil
+}
+
+func (s *ServiceImpl) GetTopXUserIDsByMatchesPlayed(ctx context.Context, topX int) ([]uint, []int, error) {
+	userIDs, matchesPlayed, err := s.repo.GetTopXUserIDsByMatchesPlayed(ctx, topX)
+	if err != nil {
+		return nil, nil, errors.Wrapf(err, "failed to get top %d userIDs by matches played", topX)
+	}
+
+	return userIDs, matchesPlayed, nil
+}
+
 func (s *ServiceImpl) UpdateStatisticsByUserIDs(ctx context.Context, userIDs []uint, result MatchResult) error {
 	var updatedStatistics []Statistic
 
@@ -31,17 +81,22 @@ func (s *ServiceImpl) UpdateStatisticsByUserIDs(ctx context.Context, userIDs []u
 
 		switch result {
 		case ResultWin:
+			stats.LossStreak = 0
 			stats.Wins++
 			stats.WinStreak++
-			stats.LossStreak = 0
+			stats.MatchesPlayed++
+			stats.WinLossRatio = float64(stats.Wins) / float64(stats.Losses)
 		case ResultLoss:
-			stats.Losses++
 			stats.WinStreak = 0
+			stats.Losses++
 			stats.LossStreak++
+			stats.MatchesPlayed++
+			stats.WinLossRatio = float64(stats.Wins) / float64(stats.Losses)
 		case ResultDraw:
-			stats.Draws++
 			stats.WinStreak = 0
 			stats.LossStreak = 0
+			stats.Draws++
+			stats.MatchesPlayed++
 		}
 
 		updatedStatistics = append(updatedStatistics, *stats)
